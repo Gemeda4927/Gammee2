576f8927b748c36bd0c5c23657b8f1098743bbda
gemada <gemadatam@gmail.com>
2024-10-02 12:42:05 +0300

    Version 1

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,14 @@ export default defineConfig({
       input: {
         main: 'src/main.jsx', // Ensure this matches your file structure
       },
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0]; // Split by package
+          }
+        }
+      }
     },
-  },
+    chunkSizeWarningLimit: 1000 // Set to 1000 kB, for example
+  }
 });
